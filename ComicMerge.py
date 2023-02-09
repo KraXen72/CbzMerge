477fa3563c602b3e2889785fd7504e6c8dfbbc74
KraXen72 <dpelech1@gmail.com>
2023-02-10 00:29:53 +0100

    add folders flag

--- ComicMerge.py
+++ ComicMerge.py
@@ -1,4 +1,5 @@
 import os
+from os.path import join
 import shutil
 import zipfile
 
@@ -13,6 +14,37 @@ def safe_remove_file(file_name):
 	if os.path.exists(file_name):
 		os.remove(file_name)
 
+def flatten_tree(abs_directory):
+	"""destructively flattens directory tree to only include files, no folders"""
+	file_dump_path = join(abs_directory, '_dir_files')
+	os.mkdir(file_dump_path)
+
+	file_counter = 1
+	for path_to_dir, subdir_names, file_names in os.walk(abs_directory, True): # move all files to 1 folder
+		for f in file_names:
+			if '.nomedia' in f:
+				os.remove(join(path_to_dir, f))
+				file_names.remove('.nomedia')
+
+		if len(subdir_names) == 0:
+			if abs_directory == path_to_dir:
+				break
+
+		for f in file_names:
+			new_name = join(path_to_dir, f"P{str(file_counter).rjust(5, '0')}")
+			os.rename(join(path_to_dir, f), join(path_to_dir, new_name))
+			shutil.move(new_name, join(file_dump_path, f))
+	
+	for subdir in os.scandir(abs_directory): # yeet all other subdirectories
+		if subdir.path.endswith('_dir_files'):
+			continue
+		shutil.rmtree(subdir.path)
+
+	new_path = os.path.split(file_dump_path)[0]
+	for f in os.listdir(file_dump_path):
+		shutil.move(join(file_dump_path, f), join(new_path, f))
+	os.rmdir(file_dump_path)
+			
 class ComicMerge:
 	def __init__(self, output_name, comics_to_merge, is_verbose=True, chapters=False, workdir='.'):
 		self.output_name = output_name
@@ -21,31 +53,42 @@ class ComicMerge:
 		self.comics_to_merge = comics_to_merge
 		self.is_verbose = is_verbose
 		self.keep_subfolders = chapters
-		self.workdir = workdir
 
+		self.workdir = workdir # comic location
+		self.abspath = os.path.dirname(os.path.abspath(__file__)) # script's location
+		if workdir is None:
+			self.workdir = self.abspath
+		
+		
 	def _log(self, msg):
 		"""only logs if verbose == True. internal"""
 		log(msg, self.is_verbose)
-
-	def _flatten_tree(self):
-		"""flattens"""
+	
+	@staticmethod
+	def _find_temp_folder():
+		base_dir = 'temp_merge'
+		mod = 0
+		temp_dir = base_dir
+		while os.path.exists(temp_dir):
+			mod += 1
+			temp_dir = base_dir + str(mod)
+		return temp_dir
 
 	def _extract_cbz(self, file_name, destination, verbose):
 		"""
 		file_name = only filename.ext, no path.
 		destination = temp folder
 		"""
-		# print("ecbz", self.workdir, file_name, destination)
-		output_dir = os.path.join(destination, os.path.splitext(file_name)[0])
+	
+		output_dir = join(destination, os.path.splitext(file_name)[0])
 		os.mkdir(output_dir)
-		log('Unzipping ' + file_name, verbose)
-
-		zip_file = zipfile.ZipFile(os.path.join(self.workdir, file_name))
+		zip_file = zipfile.ZipFile(join(self.workdir, file_name))
 		zip_file.extractall(output_dir)
 		zip_file.close()
-		# TODO deeply extract
+		flatten_tree(join(self.abspath, output_dir))
 
 	def _extract_comics(self, comics_to_extract, temp_dir):
+		print("started extracting...")
 		for file_name in comics_to_extract:
 			ComicMerge._extract_cbz(self, file_name, temp_dir, self.is_verbose)
 
@@ -53,30 +96,28 @@ class ComicMerge:
 			print("keeping subfolders for chapters")
 			# rename the folders to something more readable: ch001, ch002 etc.
 			folders = os.listdir(temp_dir)
-			last_chapter_digits = len(str(
-				len(folders)))  # number of digits the last chapter requires
+			last_chapter_digits = len(str(len(folders)))  # number of digits the last chapter requires
 			for i in range(len(folders)):
 				folder = folders[i]
 				new_name = "ch" + str(i + 1).zfill(last_chapter_digits + 1)
-				os.rename(os.path.join(temp_dir, folder),
-						  os.path.join(temp_dir, new_name))
+				os.rename(join(temp_dir, folder), join(temp_dir, new_name))
 		else:
 			# Flatten file structure (subdirectories mess with some readers)
 			files_moved = 1
 			for path_to_dir, subdir_names, file_names in os.walk(temp_dir, False):
 				for file_name in file_names:
-					file_path = os.path.join(path_to_dir, file_name)
+					file_path = join(path_to_dir, file_name)
 					ext = os.path.splitext(file_name)[1]
 					new_name = 'P' + str(files_moved).rjust(5, '0') + ext
 					log('Renaming & moving ' + file_name + ' to ' + new_name, self.is_verbose)
-					shutil.copy(file_path, os.path.join(temp_dir, new_name))
+					shutil.copy(file_path, join(temp_dir, new_name))
 					files_moved += 1
 
 				# Deletes all subdirectories (in the end we want a flat structure)
 				# This will not affect walking through the rest of the directories,
 				# because it is traversed from the bottom up instead of top down
 				for subdir_name in subdir_names:
-					shutil.rmtree(os.path.join(path_to_dir, subdir_name))
+					shutil.rmtree(join(path_to_dir, subdir_name))
 
 	def _make_cbz_from_dir(self, temp_dir):
 		self._log('Initializing cbz ' + self.output_name)
@@ -89,14 +130,14 @@ class ComicMerge:
 			for path_to_dir, subdir_names, file_names in os.walk(temp_dir):
 				page_counter = 1
 				for file_name in file_names:
-					file_path = os.path.join(path_to_dir, file_name)
+					file_path = join(path_to_dir, file_name)
 					head, tail = os.path.split(file_path)
 
 					ext = os.path.splitext(file_name)[1]
 					new_name = 'P' + str(page_counter).rjust(5, '0') + ext
 					zip_file.write(
 						file_path,
-						os.path.join(os.path.split(head)[1], new_name))
+						join(os.path.split(head)[1], new_name))
 					add_count += 1
 					page_counter += 1
 					if add_count % 10 == 0:
@@ -107,20 +148,11 @@ class ComicMerge:
 			add_count = 0
 			for path_to_dir, subdir_names, file_names in os.walk(temp_dir):
 				for file_name in file_names:
-					file_path = os.path.join(path_to_dir, file_name)
+					file_path = join(path_to_dir, file_name)
 					zip_file.write(file_path, os.path.split(file_path)[1])
 					add_count += 1
 					if add_count % 10 == 0:
 						print('> ' + str(add_count) + ' files added.', end='\r')
-
-	def _find_temp_folder():
-		base_dir = 'temp_merge'
-		mod = 0
-		temp_dir = base_dir
-		while os.path.exists(temp_dir):
-			mod += 1
-			temp_dir = base_dir + str(mod)
-		return temp_dir
 
 	@staticmethod
 	def comics_from_indices(start_idx, end_idx, workdir='.'):
